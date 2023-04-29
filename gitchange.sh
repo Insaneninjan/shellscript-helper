6ba6a12cb770c78c4b157b6cf43f2c8e8f7ffd89
naoya kojima <naoya_kojima@sora-michi.com>
2023-04-29 13:01:15 +0900

    fix: bug fix
    git config --global --replace-all コマンドのerror解消

--- gitchange.sh
+++ gitchange.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
 # Gitアカウント情報を変数に格納
-work_account=$(cat << EOS
-[user]
-    name = xxxxxxxx
-    email = xxxxxxxxxxx@xx.com
-EOS
-)
+private_name="private_name"
+private_email="private@mail.com"
 
-private_account=$(cat << EOS
-[user]
-    name = xxxxxxx
-    email = xxxxxxxxxxx@xx.com
-EOS
-)
+work_name="work_name"
+work_email="work@mail.com"
 
 # 引数によってアカウントを切り替える
 if [ "$1" = "work" ]; then
-    git config --global --replace-all "$work_account"
+    echo "Switching to work account."
+    git config --global user.name "$work_name"
+    git config --global user.email "$work_email"
     echo "Switched to work account."
 elif [ "$1" = "private" ]; then
-    git config --global --replace-all "$private_account"
+    echo "Switching to private account."
+    git config --global user.name "$private_name"
+    git config --global user.email "$private_email"
     echo "Switched to private account."
 else
     echo "Invalid argument. Please specify 'work' or 'private'."
 fi
 
 # 現在のアカウント設定を出力
+echo "Current Git Account Configuration:"
 git config --global user.name
 git config --global user.email
